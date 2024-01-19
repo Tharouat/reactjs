7196e23908ef532ef8ebc395d3b7fa0b8226c4c1
Tharouat <habka.tharouat@gmail.com>
2024-01-19 19:18:55 +0100

    my first react

--- myreact/src/components/Cardd3.jsx
+++ myreact/src/components/Cardd3.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import eventImage from './rap.jpg';
+import eventImage from './freestyle.jpg';
 const Cardd3 = () => {
   return (
     <div>
